ae68921a82b8d3b0ed582999be5fbd54f00be6b3
AnnaPengBTC <annapeng@seas.upenn.edu>
2024-06-30 11:31:59 -1000

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -27,7 +27,7 @@ def connect_with_middleware(contract_json):
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
 	bnb_url = "http://bsc-dataseed.binance.org/" #public bnb provider 
-	w3 = Web3(Web3.HTTPProvider(bnb_url))
+	w3 = Web3(HTTPProvider(bnb_url))
 	assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
 
 	# The second section requires you to inject middleware into your w3 object and
