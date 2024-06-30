420dea61555256476aee622bc4b2175d230c1a2a
AnnaPengBTC <annapeng@seas.upenn.edu>
2024-06-30 11:28:41 -1000

    Update connect_to_eth.py
    added the methods

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,7 +11,7 @@ infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 
 
 def connect_to_eth():
-	url = ""  # FILL THIS IN
+	url = "https://eth-mainnet.g.alchemy.com/v2/SOkgZqoU7p6kohfwBxhX6g-i702SwTRs"
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 	return w3
@@ -26,12 +26,20 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = 0
+	bnb_url = "http://bsc-dataseed.binance.org/" #public bnb provider 
+	w3 = Web3(Web3.HTTPProvider(bnb_url))
+	assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
 
 	# The second section requires you to inject middleware into your w3 object and
+	#inject the poa middleware for bsc
+	w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
+	contract = w3.eth.contract(address=address, abi =abi)
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	contract = 0
+	
+	#check if connection is successful
+	if not w3.is_connected():
+		raise ConnectionError("Failed to connect to the Binance Smart Chain")
 
 	return w3, contract
 
